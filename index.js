797d51328f7f0b084030aec49c7fd2abdbda2e1c
Benedict Uyioghosa <benedictuyioghosa@gmail.com>
2023-11-21 20:53:37 -0500

    Add slider functionality

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  $(".next").on("click", () => {
+    const currentImg = $(".active");
+    const nextImg = currentImg.next();
+    console.log(nextImg);
+    if (nextImg.length) {
+      currentImg.removeClass("active").css("z-index", -10);
+      nextImg.addClass("active").css("z-index", 10);
+    }
+  });
+
+  $(".prev").on("click", () => {
+    const currentImg = $(".active");
+    const prevImg = currentImg.prev();
+    if (prevImg.length) {
+      currentImg.removeClass("active").css("z-index", -10);
+      prevImg.addClass("active").css("z-index", 10);
+    }
+  });
+});
